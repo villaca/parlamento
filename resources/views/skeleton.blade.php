0085d2c37908a56b2bddd8f91656abdc1350982e
villaca <danielvillacasilva@gmail.com>
2015-06-26 16:21:07 -0300

    adicionando tudo que havia sido feito previamente

---
+++ resources/views/skeleton.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Parlamento - Home</title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li>Temas</li>
+                    <li>Debatedores Seguidos</li>
+                    <li>Mural de recados</li>
+                    <li>Alertas</li>
+                    <li>Configurações</li>
+                    <li>logout</li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+
+        </main>
+        <footer>
+
+        </footer>
+
+    </body>
+</html>
